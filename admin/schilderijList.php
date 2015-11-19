29aae5be18f71f46d6483cb0a7837263130c323b
Sjoerd Dekker <sjoerdherxen@outlook.com>
2015-11-19 13:48:55 +0100

    admin page verder verwerkt

--- admin/schilderijList.php
+++ admin/schilderijList.php
@@ -1,15 +1,19 @@
 <?php
+
 session_start();
 require './functions.php';
-if(!isLoggedIn()){
+if (!isLoggedIn()) {
     header("location: index.php");
 }
 // todo get data from db
 echo "<div id='schilderijList'>";
 foreach ($schilderijen as $schilderij) {
-    echo "<div class='schilderijListItem'>";
-    echo "<img src='".$schilderij["imgsrc"]."'>";
-    
+    echo "<a class='schilderijListItem' href='editschilderij.php?id=".$schilderij["id"]."'>";
+    echo "<img src='" . $schilderij["imgsrc"] . "'>";
+    echo "<div class='schilderijListItemInner'>";
+    echo "Naam " . $schilderij["name"] . "<br/>";
+    echo "Beschrijving " . $schilderij["description"] . "<br/>";
     echo "</div>";
+    echo "</a>";
 }
 echo "</div>";
