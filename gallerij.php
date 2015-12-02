badb204cb5950f03ddf8ada69947a708efa21b21
ruben <ruben@10.87.3.121>
2015-12-02 17:17:59 +0100

    Gallerij begin

--- gallerij.php
+++ gallerij.php
@@ -2,9 +2,41 @@
 include './htmlHelpers.php';
 renderHtmlStart("Gallerij", "");
 ?>
+<br>
 
+<!--  Sjoerd kun jij hier mischien even naar kijken. Het lijkt of hij niet de css pakt en binnen de divcontent wilt blijven.   -->
+<div class="gallerij">
+    <div class="img">
+        <a target="_parent" href="http://s9.postimg.org/6wmkzix6n/logo.jpg">
+            <img src="http://s9.postimg.org/6wmkzix6n/logo.jpg" alt="Logo" >
+        </a>
+        <div class="title">Title</div>
+    </div>
+
+    <div class="img">
+        <a target="_parent" href="http://s9.postimg.org/6wmkzix6n/logo.jpg">
+            <img src="http://s9.postimg.org/6wmkzix6n/logo.jpg" alt="Logo" >
+        </a>
+        <div class="title">Title</div>
+    </div>
+
+    <div class="img">
+        <a target="_parent" href="http://s9.postimg.org/6wmkzix6n/logo.jpg">
+            <img src="http://s9.postimg.org/6wmkzix6n/logo.jpg" alt="Logo" >
+        </a>
+        <div class="title">Title</div>
+    </div>
+
+    <div class="img">
+        <a target="_parent" href="http://s9.postimg.org/6wmkzix6n/logo.jpg">
+            <img src="http://s9.postimg.org/6wmkzix6n/logo.jpg" alt="Logo" >
+        </a>
+        <div class="title">Title</div>
+    </div>
+    <br>
+    
+</div>
 
 <?php
-
 renderHtmlEnd();
 ?>
