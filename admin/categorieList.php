7da35791f02ef1d63d1e2c254684d9c1bcb77497
Dimitry <Dimitry@145.44.65.225>
2015-12-02 12:59:46 +0100

    dingen gedaan

--- admin/categorieList.php
+++ admin/categorieList.php
@@ -15,7 +15,6 @@ echo "<a href='addCategorie.php' id='addCategorieLink'>Toevoegen</a>";
 echo "<div id='categorieList'>";
 foreach ($categorieen as $categorie) {
     echo "<a class='categorieListItem' href='editcategorie.php?id=" . $categorie["Categorie_naam"] . "'>";
-    //  echo "<div class='categorieListItemImg' style='background-image: url(\"" . $categorie["Img"] . "\");'></div>";
     echo "<div class='categorieListItemInner'>";
     echo "<span class='titel'>" . $categorie["Categorie_naam"] . "</span><br/>  ";
     echo "<span class='beschrijving'>" . $categorie["Beschrijving"] . "</span><br/>";
--- admin/editCategorie.php
+++ admin/editCategorie.php
@@ -7,27 +7,11 @@ if (!isLoggedIn()) {
 require '../htmlHelpers.php';
 renderHtmlStartAdmin("Categorie&euml;", '');
 
-$uitvoerDatabase = query("SELECT * FROM Categorie", NULL);
-?>
-<table border="1">
-<?php
-foreach ($uitvoerDatabase as $value1) {
-    foreach ($value1 as $key2 => $value2) {
-        if ($key2 == "Categorie_naam") {
-            echo "<tr><td>$value2</td>";
-        } elseif ($key2 == "Beschrijving") {
-            if ($value2 !== NULL && $value2 !== "") {
-                echo "<td>$value2</td>";
-            } else {
-                echo"<td>Geen beschijving ingevuld</td>";
-            }
-            echo'<td><a href="</td></tr>';
-        }
-    }
-}
+
+$uitvoerDatabase = query("SELECT * FROM Categorie WHERE Categorie_naam = ?", NULL);
+
 ?>
 
-</table>
     <?php
     renderHtmlEndAdmin();
 
