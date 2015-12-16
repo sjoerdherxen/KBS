d09a60e6d5f7b6690794aa9db7af57116b0a40f3
Dimitry <Dimitry@145.44.66.33>
2015-12-16 15:02:37 +0100

    commmittttt :D

---
+++ admin/editWelkomstekst.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+require 'functions.php';
+if (!isLoggedIn()) {
+    header("location: index.php");
+    exit();
+}
+require '../htmlHelpers.php';
+renderHtmlStartAdmin("Welkomstekst", "");
+
+if (isset($_POST["verzendknop"])){
+    if (isset($_POST["Beschrijving"]) && $_POST["Beschrijving"] !== ""){
+        var_dump($_POST["Beschrijving"]);
+        $invoerDatabase = [$_POST["Beschrijving"]];
+        query("UPDATE Welkomstekst SET Beschrijving = ? WHERE ID = 1", $invoerDatabase);
+    }
+}
+
+$uitvoerDatabase = query("SELECT Beschrijving FROM Welkomstekst", NULL);
+foreach ($uitvoerDatabase as $value1){
+    foreach ($value1 as $value2){
+        if (trim($value2) === ""){
+            $waarde = NULL;
+        } else {
+            $waarde = $value2;
+        }
+    }
+}
+?>
+<form action="editWelkomstekst.php" method="post">
+    <table>
+        <tr>
+            <td>
+                Tekst:
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <textarea name="Beschrijving" rows="4" cols="50"
+                          placeholder="Vul de welkomstekst (die op de hoofdpagina wordt weergeven) hier in"><?php echo $waarde; ?>
+                </textarea>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" name="empty" value="Leegmaken">
+                <input type ="submit" name="verzendkonp" value="Opslaan">
+            </td>
+        </tr>
+    </table>
+</form>
+
+
+<?php
+renderHtmlEndAdmin();
