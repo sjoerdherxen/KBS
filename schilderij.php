c3c717c58243c6c014b437a9d1fd0d14348fabe5
Sjoerd Dekker <sjoerdherxen@outlook.com>
2015-12-27 15:53:45 +0100

    schilderij.php added commentaar

--- schilderij.php
+++ schilderij.php
@@ -1,4 +1,5 @@
 <?php
+
 include './htmlHelpers.php';
 include './functions.php';
 renderHtmlStart("Schilderij", "");
@@ -8,6 +9,7 @@ renderHtmlStart("Schilderij", "");
 </script>
 
 <?php
+
 $params = array($_GET["id"]);
 
 /* TE DOEN
@@ -17,7 +19,10 @@ $params = array($_GET["id"]);
  * PRINT COMMENTAAR UIT DATABASE VAN DAT SPECIFIEKE SCHILDERIJ 
  *   */
 
-$schilderijlijst = query("SELECT S.titel, S.jaar, S.hoogte, S.breedte, S.beschrijving, C.Categorie_naam, SC.Subcategorie_naam, M.Materiaal_soort, S.img
+// eerst schilderij ophalen
+$schilderijlijst = query("SELECT S.titel, S.jaar, S.hoogte, S.breedte, S.beschrijving, 
+            C.Categorie_naam, SC.Subcategorie_naam, M.Materiaal_soort, S.img, S.prijs, S.lijst, 
+            S.passepartout, S.isStaand
  FROM Schilderij S 
   JOIN Categorie C ON C.CategorieID = S.CategorieID 
   LEFT JOIN SubCategorie SC ON SC.SubcategorieID = S.SubcategorieID 
@@ -26,160 +31,174 @@ $schilderijlijst = query("SELECT S.titel, S.jaar, S.hoogte, S.breedte, S.beschri
   WHERE S.schilderij_id = ?", $params);
 
 $schilderij = $schilderijlijst[0];
+$naam = "";
+$commentaar = "";
+$correct = true;
+
+if (isset($_POST["naam"]) && isset($_POST["commentaar"]) && checkCaptcha($_POST["g-recaptcha-response"])) {
+    $commentaar = trim($_POST["commentaar"]);
+    $naam = trim($_POST["naam"]);
+    if ($naam == "") {
+        $naamleeg = "Naam is verplicht";
+        $correct = false;
+    }
+    if ($commentaar == "") {
+        $commentaarleeg = "Commentaar is verplicht";
+        $correct = false;
+    }
+    if ($correct) {
+        $input = array($_POST["naam"], $_POST["email"], $_POST["commentaar"], $_GET["id"]);
+        query("insert into commentaar (naam_klant, email_klant, opmerking, schilderij_id) VALUES (?, ?, ?, ?)", $input);
+        ?>
+        <script>
+            alert("Commentaar is toegevoegd");
+        </script>
+        <?php
+
+    }
+}
 ?>
-<br>
-<br>
 
 <div class="schilderijimg">
-
     <img class="schilderij" alt="schilderij" src="<?php print($schilderij["img"]); ?>">
-
-    <ul class="schilderij">
-        <li>Titel: <?php print $schilderij["titel"] ?></li>
-        <li>Jaar: <?php print $schilderij["jaar"] ?></li>
-        <li>Dimensies: <?php print $schilderij["hoogte"] ?> * <?php print $schilderij["breedte"] ?></li>   
-        <li>Catagorie: <?php print $schilderij["Categorie_naam"] ?>
-
-
-            <?php
-            $result = $schilderij;
-            if (count($schilderij) == 1) {
-                print(", ");
-                print($schilderij[0]["Subcategorie_naam"]);
-            }
-            ?></li>
-        <li>Materialen: <?php print $schilderij["Materiaal_soort"] ?></li>
-
-    </ul>
-
-
-
-    <?php
-    $comments = query("SELECT * FROM commentaar C join schilderij S on Cm.Schilderij_ID = S.Schilderij_ID 
-             where schilderij_ID=?", $params);
-    $opmerkingen = $comments;
-    ?>
 </div>
-<br>
-<br>
-<div class="onderschilderij">
-    <div class="title"><?php print $schilderij["titel"] ?></div>
-    <div >
-        Beschrijving
-        <br>
-        <div class="beschrijving"><?php print $schilderij["beschrijving"] ?></div>
+<div class="row schilderijinfo">
+    <div class="col-md-4">
+        <div class="onderschilderij">
+            <h3><?php print $schilderij["titel"] ?></h3>
+            <div >
+                Beschrijving
+                <br>
+                <div class="beschrijving"><?php print $schilderij["beschrijving"] ?></div>
+
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <ul class="schilderij">
+            <li><h3>Extra info</h3></li>
+            <?php
+
+            if ($schilderij["prijs"] != "" && $schilderij["prijs"] != null) {
+                ?>
+                <li>Prijs indicatie: &euro;<?php print $schilderij["prijs"] ?></li>
+                <?php
+
+            }
+
+            if ($schilderij["jaar"] != "" && $schilderij["jaar"] != "0000" && $schilderij["jaar"] != null) {
+                ?>
+                <li>Jaar: <?php print $schilderij["jaar"] ?></li>
+                <?php
+
+            }
+            ?>
+            <li>Groote: <?php print $schilderij["hoogte"] ?> X <?php print $schilderij["breedte"] ?> cm</li>   
+            <li>Catagorie: 
+                <?php
+
+                print $schilderij["Categorie_naam"];
+                if ($schilderij["Subcategorie_naam"] != null) {
+                    print(", ");
+                    print($schilderij["Subcategorie_naam"]);
+                }
+                ?>
+            </li>
+            <li>Materiaal: <?php print $schilderij["Materiaal_soort"] ?></li>
+            <li>Lijst: <?php print $schilderij["lijst"] ? "met lijst" : "zonder lijst"; ?> </li>   
+            <li>Passepartout: <?php print $schilderij["passepartout"] ? "met passepartout" : "zonder passepartout"; ?> </li> 
+            <li>Orientatie: <?php print $schilderij["isStaand"] ? "staand" : "liggend";  ?> </li>   
+        </ul>
 
     </div>
-</div>
+    <div class="col-md-4">
+        <div class="bordercomment">
+            <div class="comments">
+                <div>
+                    <h3 class="commentaar">Commentaar</h3>
+                </div>
+                <?php
 
-<div class="bordercomment">
-    <div class="comments">
-        <div>
-            <h3 class="commentaar">Commentaar</h3>
+                $comments = query("SELECT * FROM commentaar C where schilderij_ID=?", $params);
+                foreach ($comments as $comment) {
+                    ?>
+                    <div class="comment-box">
+                        <div class="comment-naam">
+                            <?php echo $comment["Naam_klant"]; ?>
+                        </div>
+                        <div class="comment-beschrijving"><?php echo $comment["Opmerking"]; ?></div>
+                    </div>
+                    <?php
+
+                }
+                ?>
+            </div>
         </div>
 
-        <?php/* print $opmerkingen["Naam_klant"] */?>
-         <?php  /*print $opmerkingen["Opmerking"] */?> 
 
-
-
-
-
-    </div>
-
-
-    <?php
-    $naamleeg = "";
-    $naam = "";
-    $commentaar = "";
-    $commentaarleeg = "";
-    $correct = true;
-
-    if (isset($_POST["naam"]) && isset($_POST["commentaar"]) && checkCaptcha($_POST["g-recaptcha-response"])) {
-        $commentaar = trim($_POST["commentaar"]);
-        $naam = trim($_POST["naam"]);
-        if ($naam == "") {
-            $naamleeg = "Naam is verplicht";
-            $naamsucces = false;
-            $correct = false;
-        }
-        if ($commentaar == "") {
-            $commentaarleeg = "Dit veld mag niet leeg zijn";
-            $commentaarsucces = false;
-            $correct = false;
-        }
-        if ($correct) {
-            $input = array($_POST["naam"], $_POST["email"], $_POST["commentaar"], $_GET["id"]);
-            query("insert into commentaar (naam_klant, email_klant, opmerking, schilderij_id) VALUES (?, ?, ?, ?)", $input);
-            ?>
-            <script>
-                alert("Commentaar is toegevoegd");
-            </script>
-            <?php
-        }
-    }
-    ?>
-
-    <div class="capthapositie">
-        <form id="form1" name="form1" method="post" action="schilderij.php?id=<?php echo $_GET["id"]; ?>">
-            <input type="hidden"  name="id" value="<?php echo $id; ?>" />
-            <table class="comment1">
-
-
-                <tr>
-                    <td class="commentaar">Naam</td>
-                    <td>:</td>
-                    <td><input name="naam" type="text" id="naam" size="40"/>
-                        <?PHP
-                        if (isset($naamleeg)) {
-                            echo $naamleeg;
-                        }
-                        ?></td>
-                </tr>
-                <tr>
-                    <td class="commentaar">Email</td>
-                    <td>:</td>
-                    <td><input name="email" type="email" id="email" size="40" /></td>
-                </tr>
-                <tr>
-                    <td class="commentaar">Commentaar</td>
-                    <td >:</td>
-                    <td><textarea name="commentaar" cols="42" rows="4" id="opmerking" ></textarea>
-                        <?PHP
-                        if (isset($commentaarleeg)) {
-                            echo $commentaarleeg;
-                        }
-                        ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-
-                    <td>
-
-                        <div class="capthapositie1">
-
-                            <div class="g-recaptcha" data-sitekey="6LdBuRITAAAAABvjWzxipScramaFIs51kveTqRUc"></div>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td><input type="submit"  name="Submit" value="Submit">
-
-                </tr>
-
-            </table>
-
-
-
-        </form>
     </div>
 </div>
+<div class="capthapositie">
+    <form id="form1" name="form1" method="post" action="schilderij.php?id=<?php echo $_GET["id"]; ?>">
+        <input type="hidden"  name="id" value="<?php echo $id; ?>" />
+        <table class="comment1">
+            <tr>
+                <td class="commentaar">Naam</td>
+                <td>:</td>
+                <td><input name="naam" type="text" id="naam" size="40"/>
+                    <?PHP
+
+                    if (isset($naamleeg)) {
+                        echo $naamleeg;
+                    }
+                    ?></td>
+            </tr>
+            <tr>
+                <td class="commentaar">Email</td>
+                <td>:</td>
+                <td><input name="email" type="email" id="email" size="40" /></td>
+            </tr>
+            <tr>
+                <td class="commentaar">Commentaar</td>
+                <td >:</td>
+                <td><textarea name="commentaar" cols="42" rows="4" id="opmerking" ></textarea>
+                    <?PHP
+
+                    if (isset($commentaarleeg)) {
+                        echo $commentaarleeg;
+                    }
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+
+                <td>
+
+                    <div class="capthapositie1">
+
+                        <div class="g-recaptcha" data-sitekey="6LdBuRITAAAAABvjWzxipScramaFIs51kveTqRUc"></div>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td><input type="submit"  name="Submit" value="Submit">
+
+            </tr>
+
+        </table>
+
+
+
+    </form>
+</div>
+
 
 
 <?php
+
 renderHtmlEnd();
 ?>
