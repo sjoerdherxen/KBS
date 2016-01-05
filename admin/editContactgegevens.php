bb47f486079530d4d8fd0021181b05428ddae929
Dimitry <Dimitry@145.44.65.120>
2016-01-05 11:46:12 +0100

    contactgegevens laten werken

--- admin/editContactgegevens.php
+++ admin/editContactgegevens.php
@@ -9,6 +9,51 @@ renderHtmlStartAdmin("Contact", '', "contact");
 
 $uitvoerDatabase = query("SELECT * FROM schilder", NULL);
 
+$naamFout = false;
+$emailFout = false;
+
+if (isset($_POST["knopske"])) {
+    if (isset($_POST["Naam_schilder"]) && $_POST["Naam_schilder"] !== ""){
+        $doorgaan_naam = true;
+    } else {
+        $naamFout = "er moet een naam worden ingevuld";
+    }
+    if (isset($_POST["Email"]) && $_POST["Email"] !== ""){
+        $doorgaan_email = true;
+    } else {
+        $emailFout = "Er moet een email worden ingevuld";
+    }
+    if ($doorgaan_naam === true && $doorgaan_email === true){
+        if (isset($_POST["Telefoon"]) && $_POST["Telefoon"] !== ""){
+            $telefoon1 = $_POST["Telefoon"];
+        } else {
+            $telefoon1 = NULL;
+        }
+        if (isset($_POST["Woonplaats"]) && $_POST["Woonplaats"] !== ""){
+            $woonplaats1 = $_POST["Woonplaats"];
+        } else {
+            $woonplaats1 = NULL;
+        }
+        if (isset($_POST["Adres"]) && $_POST["Adres"] !== ""){
+            $adres1 = $_POST["Adres"];
+        } else {
+            $adres1 = NULL;
+        }
+        if (isset($_POST["Gebdat"]) && $_POST["Gebdat"] !== ""){
+            $gebdat1 = $_POST["Gebdat"];
+        } else {
+            $gebdat1 = NULL;
+        }
+        $naam1 = $_POST["Naam_schilder"];
+        $email1 = $_POST["Email"];
+        $invoerDatabase = [$naam1, $email1, $telefoon1, $woonplaats1, $adres1, $gebdat1];
+        var_dump($invoerDatabase);
+        query("UPDATE schilder SET Naam_schilder=?, Email=?, Telefoon=?, Woonplaats=?, Adres=?, Gebdat=?", $invoerDatabase);
+        header('location:main.php#Wijzigingen zijn opgeslagen');
+        exit();
+    }
+}
+
 foreach ($uitvoerDatabase as $value1) {
     foreach ($value1 as $key2 => $value2) {
         if ($key2 == "Woonplaats") {
@@ -36,6 +81,13 @@ foreach ($uitvoerDatabase as $value1) {
             </td>
             <td>
                 <input type="text" name="Naam_schilder" value="<?php echo $naam; ?>">
+            </td>
+            <td>
+            <?php
+            if (isset($naamFout)){
+                echo "Naam moet ingevuld worden";
+            }
+            ?>
             </td>
         </tr>
         <tr>
@@ -77,6 +129,13 @@ foreach ($uitvoerDatabase as $value1) {
             <td>
                 <input type="email" name="Email" value="<?php echo $email; ?>">
             </td>
+            <td>
+            <?php
+            if (isset($emailFout)){
+                echo "Email moet ingevuld worden";
+            }
+            ?>
+            </td>
         </tr>
         <tr>
             <td></td>
@@ -88,38 +147,6 @@ foreach ($uitvoerDatabase as $value1) {
 </form>
 
 <?php
-if (isset($_POST["knopske"])) {
-    if (isset($_POST["Naam_schilder"]) && $_POST["Naam_schilder"] !== "" && isset($_POST["Email"]) && $_POST["Email"] !== ""){
-        if (isset($_POST["Telefoon"]) && $_POST["Telefoon"] !== ""){
-            $telefoon1 = $_POST["Telefoon"];
-        } else {
-            $telefoon1 = NULL;
-        }
-        if (isset($_POST["Woonplaats"]) && $_POST["Woonplaats"] !== ""){
-            $woonplaats1 = $_POST["Woonplaats"];
-        } else {
-            $woonplaats1 = NULL;
-        }
-        if (isset($_POST["Adres"]) && $_POST["Adres"] !== ""){
-            $adres1 = $_POST["Adres"];
-        } else {
-            $adres1 = NULL;
-        }
-        if (isset($_POST["Gebdat"]) && $_POST["Gebdat"] !== ""){
-            $gebdat1 = $_POST["Gebdat"];
-        } else {
-            $gebdat1 = NULL;
-        }
-        $naam1 = $_POST["Naam_schilder"];
-        $email1 = $_POST["Email"];
-        $invoerDatabase = [$naam1, $email1, $telefoon1, $woonplaats1, $adres1, $gebdat1];
-        var_dump($invoerDatabase);
-        query("UPDATE schilder SET Naam_schilder=?, Email=?, Telefoon=?, Woonplaats=?, Adres=?, Gebdat=?", $invoerDatabase);
-        header('location:main.php#Wijzigingen zijn opgeslagen');
-        exit();
-    } else {
-        echo "hoi :D";
-    }
-}
+
 
 renderHtmlEndAdmin();
