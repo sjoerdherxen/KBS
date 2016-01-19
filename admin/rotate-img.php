24fe6d53f3788ea00ea9442031e3cb6d6e4400f5
Sjoerd Dekker <sjoerdherxen@outlook.com>
2016-01-19 17:24:02 +0100

    rotate img gefixt

--- admin/rotate-img.php
+++ admin/rotate-img.php
@@ -8,13 +8,49 @@ if (!isset($_POST["rotate_img"])) {
     $schilderijId = $_GET["id"];
     $resultImg = query("SELECT Img FROM schilderij WHERE schilderij_id = ?", array($schilderijId));
     $schilderij["Img"] = $resultImg[0]["Img"];
-    $image = "/content/uploads/" . $schilderij["Img"];
+    $imagepath = "./../content/uploads/" . $schilderij["Img"];
     $angle = 90;
     $bgd_color = 0;
-    $newimage = imagerotate($image, $angle, $bgd_color);    
-    imagealphablending($newimg, false);
-    imagesavealpha($newimage, false); 
-    imagejpeg($newimage, $image, 100);
+    $extension = strtolower(strrchr($imagepath, "."));
+
+    switch ($extension) {
+        case '.jpg':
+        case '.jpeg':
+            $image = imagecreatefromjpeg($imagepath);
+            break;
+        case '.png':
+            $image = imagecreatefrompng($imagepath);
+            break;
+        case '.gif':
+            $image = imagecreatefromgif($imagepath);
+            break;
+    }
+
+    $newimage = imagerotate($image, $angle, $bgd_color);
+    imagealphablending($newimage, false);
+    imagesavealpha($newimage, false);
+    //imagejpeg($newimage, $imagepath, 100);
+    unlink($imagepath);
+
+    switch (strtolower($extension)) {
+        case '.jpg':
+        case '.jpeg':
+            imagejpeg($newimage, $imagepath, 100);
+            break;
+        case '.png':
+            imagepng($newimage, $imagepath, 9);
+            break;
+        case '.gif':
+            imagegif($newimage, $imagepath);
+            break;
+    }
+    
+// rescale image
+    resizeImg(1150, $imagepath, $extension, 75);
+    
+    $smallimagepath = "./../content/uploads/small_" . $schilderij["Img"];
+    copy($imagepath, $smallimagepath);
+    resizeImg(228, $smallimagepath, $extension, 50);
     header("location:editschilderij.php?id=$_GET[id]");
     exit();
 }
